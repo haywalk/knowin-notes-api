25209b9d77fa44449ca8b9c8b7e41f41089a2fa3
Sawyer Stanley <sawyerscoding@gmail.com>
2024-11-03 10:07:38 -0400

    Add comments

--- sound/SoundFileException.java
+++ sound/SoundFileException.java
@@ -1,7 +1,15 @@
 package sound;
 
+/**
+ * A custom exception for indicating there is an issue with a sound file.
+ */
 public class SoundFileException extends Exception {
 
+    /**
+     * Creates a SoundFileException.
+     * 
+     * @param message The reason the exception is being thrown.
+     */
     public SoundFileException(String message){
         super(message);
     }
