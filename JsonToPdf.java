f3126fccd5af8325aa34583b432299bdf362cd1c
tbaltenkirk <113454333+tbaltenkirk@users.noreply.github.com>
2024-11-06 14:26:01 -0400

    added required reference jar files, this will need to change before merge

--- JsonToPdf.java
+++ JsonToPdf.java
@@ -11,6 +11,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 /**
  * This is the class that turns json files into pdf files for the reports after
  * playing the game.
+ * NOTE: I didn't get the imports above working, I will leave it to Gradle 
+ * magic.
  * 
  * @author Tanner Altenkirk
  * @version November 6, 2024
@@ -21,4 +23,11 @@ public class JsonToPdf {
 
     }
 
+    private void convertJsonToPdf(JsonNode data, String filepath){
+        //create a pdf document to put the results in
+        PDDocument outputDoc = new PDDocument();
+        PDPage page = new PDPage();
+
+        
+    }
 }
