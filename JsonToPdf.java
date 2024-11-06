e3ec1e3af2cc95560b2fda1742e1891449f92397
tbaltenkirk <113454333+tbaltenkirk@users.noreply.github.com>
2024-11-06 13:44:58 -0400

    Initial json to pdf converter class

---
+++ JsonToPdf.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * This is the class that turns json files into pdf files for the reports after
+ * playing the game.
+ * 
+ * @author Tanner Altenkirk
+ * @version November 6, 2024
+ */
+public class JsonToPdf {
+
+    public static void main(String[] args) {
+
+    }
+
+}
