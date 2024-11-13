24df3a8fe6196484e843ddfa9569c6e924f9134e
tbaltenkirk <113454333+tbaltenkirk@users.noreply.github.com>
2024-11-12 20:04:02 -0400

    modified method, added constructor, added method to save pdf's

--- JsonToPdf.java
+++ JsonToPdf.java
@@ -16,33 +16,37 @@ import com.fasterxml.jackson.databind.JsonNode;
  * magic.
  * 
  * @author Tanner Altenkirk
- * @version November 6, 2024
+ * @version November 12, 2024
  */
 public class JsonToPdf {
 
-    private void convertJsonToPdf(JsonNode data, String filePath){
+    private JsonToPdf(){}
+
+    public static PDDocument convertJsonToPdf(JsonNode data) throws IOException{
         //create a pdf document to put the results in
         PDDocument outputDoc = new PDDocument();
         PDPage page = new PDPage();
         outputDoc.addPage(page);
 
-        //must put next section in try/catch since PDPageContentStream throws IOException
-        //shouldnt throw exception here
-        try (PDPageContentStream contentStream = new PDPageContentStream(outputDoc, page)){ 
-            contentStream.setFont(PDType1Font.TIMES_BOLD, 12);
-            contentStream.newLineAtOffset(50, 700);
+        //Exception thrown by PDPageContentStream to be handled elsewhere
+        PDPageContentStream contentStream = new PDPageContentStream(outputDoc, page); 
+        contentStream.setFont(PDType1Font.TIMES_BOLD, 12); //for some reason, all font names throw unresolved errors... this will probably need to be fixed lol
+        contentStream.newLineAtOffset(50, 700);
 
-            //field names in order should be: 'Date', 'Time', 'GameMode', 'clef', 'notetypes', 'Gameduration', 'notesInGame', 'timePerNote', 'accuracy'
-            Iterator<String> fieldNames = data.fieldNames();
-            while(fieldNames.hasNext()){
-                String currentField = fieldNames.next();
-                contentStream.showText(currentField + ": " + data.get(currentField).asText());
-                contentStream.newLineAtOffset(0, -15);
-            }
-            contentStream.endText();
+        //field names in order should be: 'Date', 'Time', 'GameMode', 'clef', 'notetypes', 'Gameduration', 'notesInGame', 'timePerNote', 'accuracy'
+        Iterator<String> fieldNames = data.fieldNames();
+        while(fieldNames.hasNext()){
+            String currentField = fieldNames.next();
+            contentStream.showText(currentField + ": " + data.get(currentField).asText());
+            contentStream.newLineAtOffset(0, -15);
         }
+        contentStream.endText();
+        return outputDoc;
+    }
+
+    //this method takes in a JsonNode and a file path and saves the data as a pdf to that file location
+    public void savePdf(JsonNode data, String filePath) throws IOException{
+        PDDocument outputDoc = convertJsonToPdf(data);
         outputDoc.save(new File(filePath));
     }
 }
-//separate convert and save methods
-//make public static, empty private constructor
